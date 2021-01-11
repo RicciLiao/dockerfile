f5ae14e3ffb3ca93320dbef329b8050c16ad7540
Ricci Liao <ricciliao@aslcn.com.cn>
2021-01-11 16:52:16 +0800

    create build.sh for building docker images

---
+++ build.sh
@@ -0,0 +1,32 @@
+set -o verbose
+base_path=$(pwd)
+build_target=$1
+image_name=$2
+build_target_path="$base_path"/"$build_target"
+
+if [ "$build_target" = "1-RocketMQ" ]; then
+  echo "not ready for this dockerfile"
+  return
+fi
+
+if [ -z "$image_name" ]; then
+  echo "wrong image name"
+  return
+fi
+
+if [ -z "$build_target" ]; then
+  echo "wrong build target"
+  return
+elif [ ! -d "$build_target_path" ]; then
+  echo "target path not exsit"
+  return
+elif [ ! -s "$build_target_path"/Dockerfile ]; then
+  echo "dockerfile not exsit"
+  return
+fi
+
+cp "$build_target_path"/Dockerfile "$base_path"/Dockerfile
+
+docker build --no-cache -t "$image_name" .
+
+rm -f "$base_path"/Dockerfile
